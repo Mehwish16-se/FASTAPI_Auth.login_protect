06db80c264114810ddee7ffaafbedf6f825a32bd
Mehwish16-se <mehwishrehman019@gmail.com>
2026-09-01 13:40:44 +0500

    Stage 0: Supabase auth setup

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "FlyRank Auth API is running"
+    }
